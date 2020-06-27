a36582468723d4001e6ff7c967dab308fe7542fd
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2020-06-27 02:23:13 +0200

    Update Observable_.hpp

--- source/library/Observable_.hpp
+++ source/library/Observable_.hpp
@@ -5,15 +5,18 @@
 #include <unordered_set>    // std::unordered_set
 
 namespace event_handling_support {
-    $use_cppx( No_copy, Type_ );
+    $use_cppx( As_derived_, No_copy, Type_ );
     $use_std( unordered_set );
 
-    template< class Derived_param, class Observer_interface_param >
+    template<
+        class Derived_param,                    // Use e.g. `As_derived_<D>` to create this type.
+        class Observer_interface_param
+        >
     class Observable_:
         public No_copy
     {
     public:
-        using Derived               = Derived_param;
+        using Derived               = typename Derived_param::Derived;
         using Observer_interface    = Observer_interface_param;
 
     private:
